b41a2f62fc25ecbd839b47df2a7f3f4aa4f48a3c
Bruno Rossi <brunorossicarmo@usp.br>
2019-07-12 21:19:29 -0300

    Documentos - Scripts e Unity Save - Mov.
    Neste commit apresenta os scripts para movimentação do personagem e câmera seguindo jogador. Também contém o save da cena do Unity.

---
+++ Mythology Boys/Scripts/Scripts de Movimentação/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerObject;
+    public float distanceFromObject = 10f;
+    void Update()
+    {
+        Vector3 lookOnObject = playerObject.position - transform.position;
+        lookOnObject = playerObject.position - transform.position;
+        transform.forward = lookOnObject.normalized;
+        Vector3 playerLastPosition;
+        playerLastPosition = playerObject.position - lookOnObject.normalized * distanceFromObject;
+        playerLastPosition.y = playerObject.position.y + distanceFromObject/4;
+        transform.position = playerLastPosition;
+    }
+}
---
+++ Mythology Boys/Scripts/Scripts de Movimentação/Movimento_Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimento_Player : MonoBehaviour
+{
+
+    float _velocidade;
+    float _girar;
+
+    void Start()
+    {
+        _velocidade = 10.0F;
+        _girar = 140.0F;
+    }
+
+    // 1 update/frame
+    void Update()
+    {
+        float translate = (Input.GetAxis("Vertical") * _velocidade) * Time.deltaTime;
+        float rotate = (Input.GetAxis("Horizontal") * _girar) * Time.deltaTime;
+
+        transform.Translate(0, 0, translate);
+        transform.Rotate(0, rotate, 0);
+    }
+
+
+}
